1e0ca082a42824cf234c60f282963f7342409581
aristidei <a.isingizwe@alustudent.com>
2024-11-05 16:25:13 +0200

    about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,17 @@
 import LeftSection from "../components/aboutPage/left";
 import MiddleSection from "../components/aboutPage/middle";
 import RightSection from "../components/aboutPage/right";
+import SkillsSection from "../components/homePage/skills";
 
 export default function AboutPage() {
   return (
-    <article className="padd w-full h-[120vh] flex justify-between gap-6 py-12">
-      <LeftSection />
-      <MiddleSection />
-      <RightSection />
+    <article>
+      <SkillsSection />
+      <article className="padd w-full h-[120vh] flex justify-between gap-6 py-12">
+        <LeftSection />
+        <MiddleSection />
+        <RightSection />
+      </article>
     </article>
   );
 }
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,14 +1,12 @@
 import AboutSection from "../components/homePage/aboutSection";
 import HeroSection from "../components/homePage/hero";
 import ProjectsSection from "../components/homePage/projects";
-import SkillsSection from "../components/homePage/skills";
 
 export default function HomePage() {
   return (
     <section>
       <HeroSection />
       <AboutSection />
-      <SkillsSection />
       <ProjectsSection />
       <div className="h-screen" />
     </section>
